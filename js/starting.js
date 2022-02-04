30176930792d640868c95439669023f4bdb294d1
npoliveira <npoliveira@gmail.com>
2022-02-04 21:13:52 +0100

    Included a new JS file. - Added BMI calc: * for and if - Used functions: * querySelectorAll * textContent
    Obs: Adding missing files

---
+++ js/starting.js
@@ -0,0 +1,32 @@
+let pacientes = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < pacientes.length; i++) {
+
+    let paciente = pacientes[i];
+
+    let tdPeso = paciente.querySelector(".info-peso");
+    let peso = tdPeso.textContent;
+
+    let tdAltura = paciente.querySelector(".info-altura");
+    let altura = tdAltura.textContent;
+
+    let tdImc = paciente.querySelector(".info-imc");
+
+    let pesoEhValido = true;
+    let alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 500) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("dado-invalido");
+    }
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Invalida!"
+        paciente.classList.add("dado-invalido");
+    }
+    if (alturaEhValida && pesoEhValido) {
+        let imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
